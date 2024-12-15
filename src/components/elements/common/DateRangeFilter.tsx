a77608540c2b86c9882c26c2896a14a45612756e
Emanuel Avila <avilaemanueel@gmail.com>
2024-12-15 15:56:43 -0300

    upgrade to nextjs 15

--- src/components/elements/common/DateRangeFilter.tsx
+++ src/components/elements/common/DateRangeFilter.tsx
@@ -1,15 +1,9 @@
 import { SelectNative } from "@/components/ui/select-native";
 
-export function DateRangeFilter({
-  value,
-  setValue,
-}: {
-  value: string;
-  setValue: React.Dispatch<React.SetStateAction<string>>;
-}) {
+export function DateRangeFilter() {
   return (
     <div className="space-y-2">
-      <SelectNative value={value} onChange={(e) => setValue(e.target.value)}>
+      <SelectNative>
         <option value="24h">Last 24 hours</option>
         <option value="7d">Last 7 days</option>
         <option value="30d">Last 30 days</option>
