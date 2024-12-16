ee4a2ff7fc473366c01d5ff749edf40623feb394
Emanuel Avila <avilaemanueel@gmail.com>
2024-12-15 21:17:30 -0300

    update import

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,8 +4,8 @@ import { NuqsAdapter } from "nuqs/adapters/next/app";
 import type { PropsWithChildren } from "react";
 import React from "react";
 
-import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "sonner";
 import { ThemeProvider } from "./ThemeProvider";
 
 export function Providers({ children }: PropsWithChildren) {
@@ -20,7 +20,12 @@ export function Providers({ children }: PropsWithChildren) {
         <NuqsAdapter>
           <TooltipProvider>{children}</TooltipProvider>
         </NuqsAdapter>
-        <Toaster position="top-right" richColors closeButton duration={3500} />
+        <Toaster
+          position="bottom-right"
+          richColors
+          closeButton
+          duration={3500}
+        />
       </ThemeProvider>
     </React.Fragment>
   );
