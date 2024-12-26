85f7dc9ade182c47a2e90433ac2da032bdf3ae73
Emanuel Avila <avilaemanueel@gmail.com>
2024-12-25 21:58:28 -0300

    update base api and route

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,8 +4,12 @@ import { env } from "@/env.mjs";
 
 export const api = axios.create({
   baseURL: env.NEXT_PUBLIC_BASE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    // Authorization:
+    // "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NjI2OTQxOCIsImlhdCI6MTczNTE3MjM2NTM0NywiZXhwIjoxNzM1Nzc3MTY1MzQ3LCJ1c2VyIjp7Im5hbWUiOiJFbWFudWVsIEF2aWxhIiwiZ2l0aHViX2lkIjoiNzYyNjk0MTgifX0.kdEUlz5B96_DkvtHfPg1xn2HqdpEhAb3JS3ZITaapkA",
+  },
 });
 
 // Exporting routes definition
-export const AVARAGE_COMMENT_SCORE = (repository_id: string): string =>
-  `gh-comments/${repository_id}/average-score`;
+export const USER_PROFILE_ROUTE = (): string => "/user/profile";
