6ecb216d0fa753bb8d2ddfb584f62771d13962c8
Emanuel Avila <avilaemanueel@gmail.com>
2024-12-26 13:21:33 -0300

    add a refetchInterval

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -5,6 +5,7 @@ export function userService() {
   const { data: user, isLoading: loading } = useQuery({
     queryKey: ["user-profile"],
     queryFn: getUserProfile,
+    refetchInterval: 1000 * 60 * 15, // 15 minutes
   });
 
   return {
