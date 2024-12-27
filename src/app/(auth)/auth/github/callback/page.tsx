d643194f1da28219802ff532d959aaabc63fb92b
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-12-27 12:15:25 -0300

    feat: add GitHub OAuth callback component for handling authentication response

---
+++ src/app/(auth)/auth/github/callback/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect } from "react";
+import { parseAsString, useQueryStates } from "nuqs";
+import { useRouter } from "next/navigation";
+import { api } from "@/lib/api";
+import { githubAuthCallbackService } from "@/services/auth";
+import { LoaderCircle } from "lucide-react";
+
+export default function GithubCallback() {
+  const [queries, _] = useQueryStates(
+    {
+      code: parseAsString,
+    },
+    {
+      shallow: false,
+    }
+  );
+
+  const router = useRouter();
+  const { data, isLoading, isError } = githubAuthCallbackService(
+    queries.code || ""
+  );
+
+  useEffect(() => {
+    if (data) {
+      const { access_token } = data;
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+      localStorage.setItem("@Auth:token", access_token);
+
+      router.push("/app");
+    }
+  }, [data, router]);
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center p-4">
+        <LoaderCircle className="h-8 w-8 animate-spin mb-4" />
+        <p className="text-lg font-medium">Loading...</p>
+      </div>
+    );
+  }
+
+  if (isError || !data) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center p-4">
+        <p className="text-lg font-medium mb-2">Failed to load data.</p>
+        <p className="text-sm">Please try again later.</p>
+      </div>
+    );
+  }
+
+  return null;
+}
