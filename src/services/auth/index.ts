6a237113f15d617f04883204e46b9fe71f3c878f
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-12-27 12:14:55 -0300

    feat: add GitHub authentication service functions

---
+++ src/services/auth/index.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubAuth, githubAuthCallback } from "./action";
+
+export function githubAuthService() {
+  const { data: url, isLoading } = useQuery({
+    queryKey: ["github-auth"],
+    queryFn: githubAuth,
+  });
+
+  return {
+    url,
+    isLoading,
+  };
+}
+
+export function githubAuthCallbackService(code: string) {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["github-auth-callback", code],
+    queryFn: () => githubAuthCallback(code),
+    enabled: !!code,
+  });
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+}
