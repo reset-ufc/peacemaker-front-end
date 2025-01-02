a278c737dff4b3ae769bed3a516a309df7b284d3
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-02 17:55:02 -0300

    update user profile page

--- src/app/(dashboard)/settings/profile/page.tsx
+++ src/app/(dashboard)/settings/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 import {
   Card,
   CardContent,
@@ -8,16 +8,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormDescription,
+  FormField,
+  FormItem,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
+import { userService } from "@/services/user";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 /**
  * Force the page to be static and only change with a new build.
@@ -34,52 +37,101 @@ export const dynamic = "force-static";
  * https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
  */
 
+const formSchema = z.object({
+  fullName: z.string().min(2),
+  username: z.string().min(2),
+  email: z.string().email().optional(),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
 export default function Page() {
-  const [date, setDate] = useState<Date>();
+  const { user } = userService();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: user?.profile?.email ?? "",
+      username: user?.profile?.username ?? "",
+      fullName: user?.profile?.name ?? "",
+    },
+  });
+
+  async function handleSubmit(data: FormData) {
+    console.log(data);
+
+    setIsLoading(true);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1200));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
-    <Card>
+    <Card className="rounded-md mx-auto border-none">
       <CardHeader>
         <CardTitle>User</CardTitle>
         <CardDescription>
           Used to identify your account and personalize your experience.
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-6">
-        <div className="grid gap-4 md:grid-cols-2">
-          <div className="space-y-2">
-            <Label htmlFor="firstName">First name</Label>
-            <Input id="firstName" placeholder="Juan" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="lastName">Last name</Label>
-            <Input id="lastName" placeholder="Very good" />
-          </div>
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="birthDate">Birth date</Label>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                id="birthDate"
-                variant="outline"
-                className="w-full justify-start text-left font-normal md:w-[280px]"
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date ? format(date, "PPP") : "Pick a date"}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
+      <CardContent className="space-y-6 max-w-xl">
+        <Form {...form}>
+          <form
+            className="space-y-6"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <div>
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="firstName">First Name</Label>
+                    <Input id="firstName" placeholder="First Name" {...field} />
+                  </FormItem>
+                )}
               />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <Button>Save changes</Button>
+
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="username">Username</Label>
+                    <Input
+                      id="username"
+                      placeholder="Username"
+                      disabled
+                      {...field}
+                    />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" placeholder="Email" {...field} />
+                    <FormDescription>
+                      The change of email on our platform is not reflected on
+                      Github.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button>{isLoading ? "Saving..." : "Save Changes"}</Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
