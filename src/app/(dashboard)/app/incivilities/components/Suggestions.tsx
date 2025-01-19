4f17b9ba263e71c6eb32634d269d9d1dce88bbb9
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-19 15:18:19 -0300

    update component

--- src/app/(dashboard)/app/incivilities/components/Suggestions.tsx
+++ src/app/(dashboard)/app/incivilities/components/Suggestions.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 
+import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
+
 import { SubmitButton } from "@/components/elements/common/SubmitButton";
 import { Separator } from "@/components/ui/separator";
 import { useNotification } from "@/hooks/use-notification";
@@ -36,46 +38,59 @@ export function Suggestions({ suggestions }: SuggestionsProps) {
   }
 
   return (
-    <div className="mt-4 flex flex-col gap-4 rounded-md border px-6 py-4">
+    <div className="mt-4 flex flex-col gap-4 px-6 py-4">
       <h2 className="text-md font-bold">Suggestions for fixing your comment</h2>
 
       <Separator orientation="horizontal" />
 
-      <form className="space-y-8" onSubmit={approveSuggestion}>
-        <div className="flex flex-row items-center justify-between">
-          <div className="flex flex-row gap-4">
-            <input
-              id="suggestion"
-              type="radio"
-              value={suggestions.corrected_comment}
-              checked={selectedSuggestion === suggestions.corrected_comment}
-              onChange={() =>
-                setSelectedSuggestion(suggestions.corrected_comment)
-              }
-              disabled={isLoading}
-              required
-            />
+      <form className="space-y-12" onSubmit={approveSuggestion}>
+        {suggestions ? (
+          <>
+            <div className="group">
+              <div className="flex flex-row gap-4">
+                <input
+                  id="suggestion"
+                  type="radio"
+                  value={suggestions.corrected_comment}
+                  checked={selectedSuggestion === suggestions.corrected_comment}
+                  onChange={() =>
+                    setSelectedSuggestion(suggestions.corrected_comment)
+                  }
+                  disabled={isLoading}
+                  required
+                />
 
-            <label
-              htmlFor="suggestion"
-              className={cn(
-                "text-base",
-                isLoading && "cursor-not-allowed text-muted-foreground",
-              )}
-            >
-              <MarkdownDisplay text={suggestions.corrected_comment} />
-            </label>
+                <label
+                  htmlFor="suggestion"
+                  className={cn(
+                    "text-base",
+                    isLoading && "cursor-not-allowed text-muted-foreground",
+                  )}
+                >
+                  <MarkdownDisplay text={suggestions.corrected_comment} />
+                </label>
+              </div>
+
+              {/* TODO: Leave the element visible when hovering over the text, but without moving the text. */}
+              <div className="ms-10 hidden flex-row gap-2 rounded-lg group-hover:visible group-hover:flex">
+                <button className="rounded-lg p-1 hover:bg-muted" type="button">
+                  <ThumbsUpIcon className="size-5 text-foreground" />
+                </button>
+                <button className="rounded-lg p-1 hover:bg-muted" type="button">
+                  <ThumbsDownIcon className="size-5 text-foreground" />
+                </button>
+              </div>
+            </div>
+
+            <SubmitButton type="submit" isSubmitting={isLoading} size="sm">
+              Approve
+            </SubmitButton>
+          </>
+        ) : (
+          <div className="p-8 text-center text-muted-foreground">
+            No suggestions found
           </div>
-
-          {/* <SuggestionActions
-            selectedSuggestion={selectedSuggestion}
-            suggestion={suggestions.corrected_comment}
-          /> */}
-        </div>
-
-        <SubmitButton type="submit" isSubmitting={isLoading}>
-          Approve
-        </SubmitButton>
+        )}
       </form>
     </div>
   );
