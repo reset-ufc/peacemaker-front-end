37ace30d4ae9a06848d75b7f7de677d52d1b6a08
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-19 13:56:07 -0300

    remove sidebar usage

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,9 @@
 import { AppHeader } from "@/components/elements/layout/AppHeader";
-import { AppSidebar } from "@/components/elements/layout/AppSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
       <main className="w-full">
         <AppHeader />
         {children}
