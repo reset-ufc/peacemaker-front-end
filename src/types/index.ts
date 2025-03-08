914ed779dff7cc9c17f763ac618da2f28211f6ab
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-07 21:09:52 -0300

    adiciona interfaces Comment e CommentParent para estruturação de dados de comentários

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export interface Comment {
+  _id: string;
+  gh_comment_id: string;
+  content: string;
+  comment_created_at: string;
+  author_id: string;
+  parent: CommentParent;
+  repository_fullname: string;
+  is_repository_private: boolean;
+  repository_owner: string;
+  toxicity_score: number;
+  toxicity_analyzed_at: string;
+  flagged: boolean;
+  classification: string;
+  solutioned: boolean;
+  solution: string;
+  solution_analyzed_at: any;
+  solution_id: any;
+  event_type: string;
+  installation_id: number;
+  moderated: boolean;
+  moderation_action: any;
+  __v: number;
+}
+
+export interface CommentParent {
+  type: string;
+  gh_parent_id: number;
+  title: string;
+  url: string;
+}
