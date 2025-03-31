9effbca89ad9738e500745510ddc03a38ad6cdfa
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-31 00:01:12 -0300

    refatora a página Repositories para usar React Query no gerenciamento de dados

--- src/pages/app/Repositories.page.tsx
+++ src/pages/app/Repositories.page.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-
+import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import { RepositoriesTable } from "@/components/layout/RepositoriesTable";
@@ -7,28 +6,36 @@ import { api } from "@/lib/api";
 import { Repository } from "@/types";
 
 export function RepositoriesPage() {
-  const [repos, setRepos] = useState<Array<Repository>>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
+  const query = useQuery({
+    queryKey: ["repositories"],
+    queryFn: async () => {
       const t = localStorage.getItem("access_token");
-
+      await new Promise(resolve => setTimeout(resolve, 1500));
       const request: AxiosResponse<{ repositories: Array<Repository> }> =
         await api.get("/api/repositories", {
           headers: {
             Authorization: `Bearer ${t}`,
           },
         });
+      return request.data.repositories;
+    },
+    refetchOnWindowFocus: true,
+    refetchOnMount: false,
+  });
 
-      setRepos(request.data.repositories);
-    };
+  // Create a Loading component
+  if (query.isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    fetchData();
-  }, []);
+  // Handle errors with a fallback component
+  if (query.isError) {
+    return <div>Error: {query.error.message}</div>;
+  }
 
   return (
     <main className="h-[calc(100vh-4rem)] w-full p-8">
-      <RepositoriesTable repositories={repos} />
+      <RepositoriesTable repositories={query.data ?? []} />
     </main>
   );
 }
