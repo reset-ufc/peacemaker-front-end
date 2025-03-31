a83d4c7024d6ebd2704769ab9bc33d7b19949e05
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-31 00:01:17 -0300

    adiciona suporte ao React Query no provedor de contexto e integra ReactQueryDevtools

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -3,6 +3,8 @@
 import type { PropsWithChildren } from "react";
 import React from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { NuqsAdapter } from "nuqs/adapters/react";
 import { Toaster } from "sonner";
 
@@ -10,24 +12,29 @@ import { AuthProvider } from "./AuthProvider";
 import { ThemeProvider } from "./ThemeProvider";
 
 export function Providers({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient();
+
   return (
     <React.Fragment>
-      <AuthProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          storageKey="theme"
-        >
-          <NuqsAdapter>{children}</NuqsAdapter>
-          <Toaster
-            position="bottom-right"
-            richColors
-            closeButton
-            duration={3500}
-          />
-        </ThemeProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            storageKey="theme"
+          >
+            <NuqsAdapter>{children}</NuqsAdapter>
+            <Toaster
+              position="bottom-right"
+              richColors
+              closeButton
+              duration={3500}
+            />
+          </ThemeProvider>
+        </AuthProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </React.Fragment>
   );
 }
