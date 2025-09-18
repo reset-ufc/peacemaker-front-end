7f9a3b631bb1eb38f9662a413371bb94d4abf4b5
Emanuel Avila <avilaemanueel@gmail.com>
2025-09-17 23:17:16 -0300

    organize imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { NotFound } from "./components/layout/NotFound";
 import { Introduction } from "./docs/introduction";
+import { HomePage } from "./pages/Index.page";
+import { PrivacyPage } from "./pages/Privacy.page";
+import { TermsPage } from "./pages/Terms.page";
 import { AppLayout } from "./pages/_layout/App.layout";
 import { AuthLayout } from "./pages/_layout/Auth.layout";
 import { PublicLayout } from "./pages/_layout/Public.layout";
@@ -10,9 +13,6 @@ import { IncivilitiesPage } from "./pages/app/Incivilities.page";
 import { RepositoriesPage } from "./pages/app/Repositories.page";
 import { GithubAuthPage } from "./pages/auth/GithubAuth.page";
 import { GithubAuthCallback } from "./pages/auth/GithubAuthCallback.page";
-import { HomePage } from "./pages/Index.page";
-import { PrivacyPage } from "./pages/Privacy.page";
-import { TermsPage } from "./pages/Terms.page";
 import { Providers } from "./providers";
 import "./utils/i18n";
 
