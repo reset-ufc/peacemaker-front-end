e77b9a2c6fda9004bdf61417e52a79902c572203
Emanuel Avila <avilaemanueel@gmail.com>
2025-09-17 23:19:02 -0300

    TODO: crash issue on firefox
    the component `Features` cause crash when the website is access from firefox

--- src/pages/Index.page.tsx
+++ src/pages/Index.page.tsx
@@ -1,11 +1,10 @@
-import { Features } from "@/components/layout/Features";
 import { Footer } from "@/components/layout/Footer";
 import { Team } from "@/components/layout/Team";
 
 export function HomePage() {
   return (
     <>
-      <Features />
+      {/* <Features /> */}
       <Team />
       <Footer />
     </>
