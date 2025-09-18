5e6b83107461937a3202dcbea7b788a3c131a0ef
Emanuel Avila <avilaemanueel@gmail.com>
2025-09-17 23:17:38 -0300

    update declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  // strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  VITE_APP_URL: string;
+  VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
